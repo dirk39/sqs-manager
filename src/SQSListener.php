517a615ddf27324d251ad0d5a46e84fcc2121a1c
Dirk39 <andreaquin1990@gmail.com>
2018-03-19 23:18:10 +0100

    SQSListener::run is able to get message from a queue, managed e delete them

--- src/SQSListener.php
+++ src/SQSListener.php
@@ -1,14 +1,18 @@
 <?php
 
 use Aws\Sqs\SqsClient;
+use \Symfony\Component\Filesystem\LockHandler;
 
 class SQSListener
 {
   /** @var Aws\Sqs\SqsClient */
   private $client;
+  /** @var \Symfony\Component\Filesystem\LockHandler  */
   private $lockHandler;
 
-  public function __construct($appId, $appSecret, $region = 'eu-west-1')
+  private $listenerConfig = [];
+
+  public function __construct($appId, $appSecret, $region = 'eu-west-1', array $listenerConfig = [])
   {
     $this->client = SqsClient::factory([
       'key' => $appId,
@@ -16,19 +20,51 @@ class SQSListener
       'region' => $region
     ]);
 
-    $this->lockHandler = new \Symfony\Component\Filesystem\LockHandler($this->getTempFileName());
+    $this->lockHandler = new LockHandler($this->getTempFileName());
+
+    $this->listenerConfig = $listenerConfig;
   }
 
   /**
    * @param $queueName
    * @param bool $alwaysListening
    */
-  public function run($queueName, $alwaysListening = false)
+  public function run($queueName, array $callback, $alwaysListening = false)
   {
     if($alwaysListening && !$this->setPermanentListener())
     {
       return;
     }
+
+    $queueUrl = $this->getQueueUrl($queueName);
+    $config = ['QueueUrl' => $queueUrl] + $this->listenerConfig;
+
+    do
+    {
+      /** @var Guzzle\Service\Resource\Model $response */
+      $response = $this->client->receiveMessage($config);
+      $messages = $response['Messages'];
+      foreach ($messages as $message) {
+        call_user_func($callback, $message);
+        $this->client->deleteMessage([
+          'QueueUrl' => $queueUrl,
+          'ReceiptHandle' => $message['ReceiptHandle']
+        ]);
+      }
+    }while($alwaysListening);
+  }
+
+  protected function getQueueUrl($queueName)
+  {
+    if(filter_var($queueName,FILTER_VALIDATE_URL)) {
+      return $queueName;
+    }
+
+    $result = $this->client->getQueueUrl([
+      'QueueName' => $queueName
+    ]);
+
+    return $result['QueueUrl'];
   }
 
   protected function setPermanentListener()
@@ -49,4 +85,18 @@ class SQSListener
     return sha1(__CLASS__).'.lock';
   }
 
+  public function setConfig($key, $value)
+  {
+    if(null === $value && isset($this->listenerConfig[$key]))
+    {
+      unset($this->listenerConfig[$key]);
+    }
+
+    $this->listenerConfig[$key] = $value;
+  }
+
+  public function getConfig($key, $default = null)
+  {
+    return array_key_exists($key, $this->listenerConfig)? $this->listenerConfig[$key]: $default;
+  }
 }
