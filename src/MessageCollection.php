01c956d1e0b50e93c1529fd929a647c667bfd0ff
Dirk39 <andreaquin1990@gmail.com>
2018-03-27 22:51:30 +0200

    rewrite collection

--- src/MessageCollection.php
+++ src/MessageCollection.php
@@ -9,42 +9,44 @@
 class MessageCollection implements Iterator
 {
   private $messages = [];
+  private $position = 0;
 
   public function __construct(array $messages, \Aws\Sqs\SqsClient $client)
   {
     $this->init($messages, $client);
+    $this->position;
   }
 
   protected function init(array $messages, \Aws\Sqs\SqsClient $client)
   {
     foreach ($messages as $message)
     {
-      $this->messages[] = new \Message($message, $client);
+      $this->messages[] = new \Message($message, 'lol',$client);
     }
   }
 
   public function key()
   {
-    return key($this->messages);
+    return $this->position;
   }
 
   public function current()
   {
-    return current($this->messages);
+    return $this->messages[$this->position];
   }
 
   public function next()
   {
-    return next($this->messages);
+    ++$this->position;
   }
 
   public function rewind()
   {
-    return reset($this->messages);
+    $this->position = 0;
   }
 
   public function valid()
   {
-    return isset($this->messages[$this->key()]);
+    return isset($this->messages[$this->position]);
   }
 }
